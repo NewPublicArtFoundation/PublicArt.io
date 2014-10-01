a49804f00e43a0534d6f4480533f327c235c2ee0
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-01 09:22:20 -0400

    Style the h1

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
   resources :locations
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'locations#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
