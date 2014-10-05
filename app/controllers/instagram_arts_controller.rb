afc4f515508d51e0dc32974f7267f286937b5fdc
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-05 19:51:23 -0400

    Save a no limit

--- app/controllers/instagram_arts_controller.rb
+++ app/controllers/instagram_arts_controller.rb
@@ -7,7 +7,7 @@ class InstagramArtsController < ApplicationController
     if params[:search].present?
       @instagram_arts = InstagramArt.near(params[:search], 50, :order => :distance)
     else
-      @instagram_arts = InstagramArt.all
+      @instagram_arts = InstagramArtusers.all(:limit => 20)
     end
   end
 
