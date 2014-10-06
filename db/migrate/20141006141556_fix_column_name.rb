17a78167cf8454c01f5623fccebe8b58f9f985c3
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-06 10:29:01 -0400

    Correct the columns

--- db/migrate/20141006141556_fix_column_name.rb
+++ db/migrate/20141006141556_fix_column_name.rb
@@ -1,6 +1,6 @@
 class FixColumnName < ActiveRecord::Migration
   def change
-    rename_column :instagram_arts :longitude :longitude
-    rename_column :instagram_arts :location_lat :latitude
+    rename_column :instagram_arts, :location_lon, :longitude
+    rename_column :instagram_arts, :location_lat, :latitude
   end
 end
