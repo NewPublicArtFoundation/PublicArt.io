47e475c9a3924fc4929197f13463f2c65c3c12f7
rememberlenny <leonard_bogdonoff@condenast.com>
2014-10-07 14:34:24 -0400

    Save the new location endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :locations
   resources :instagram_arts
 
+  get 'instagram_arts/location_search' => 'instagram_arts#indexlocation'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
