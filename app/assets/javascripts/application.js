d3cac49886810bcca96e8b3decd03461a04f6b5b
rememberlenny <leonard_bogdonoff@condenast.com>
2014-10-08 13:06:17 -0400

    Update the foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require foundation
 //= require jquery
 //= require jquery_ujs
 //
@@ -17,3 +17,5 @@
 //
 //= require turbolinks
 //= require_tree .
+
+$(document).foundation();
