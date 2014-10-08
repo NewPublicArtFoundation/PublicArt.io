daf51074735b070e000426824a6a1ddb821fcd3a
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-07 21:07:58 -0400

    GP current location

---
+++ public/assets/js/GP-current-location.js
@@ -0,0 +1,23 @@
+var GP = {} || GP;
+
+GP.initGeolocation = function(){
+    if( navigator.geolocation ){
+        // Call getCurrentPosition with success and failure callbacks
+        navigator.geolocation.getCurrentPosition( GP.success, GP.fail );
+    } else {
+        alert("Sorry, your browser does not support geolocation services.");
+    }
+}
+
+GP.success = function(position){
+    console.log('long', position.coords.longitude);
+    console.log('lat', position.coords.latitude);
+}
+
+GP.fail = function(){
+    // Could not obtain location
+}
+
+$('.GP_get_location').on('click', function(){
+    initGeolocation();
+});
