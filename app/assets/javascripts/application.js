b74608d60cd1af5579c763160df8911ecab89a48
rememberlenny <leonard_bogdonoff@condenast.com>
2014-10-08 13:36:47 -0400

    Define jquery before foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,10 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require foundation
 //= require jquery
 //= require jquery_ujs
 //
+//= require foundation
 //= require GP-current-location
 //
 //= require turbolinks
