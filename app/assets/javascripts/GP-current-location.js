a72fea92af91424fb6937051a79ffcf8ce7ffc57
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-07 21:20:26 -0400

    Save update

--- app/assets/javascripts/GP-current-location.js
+++ app/assets/javascripts/GP-current-location.js
@@ -1,24 +1,29 @@
-var GP = {} || GP;
+(function(){
 
-GP.initGeolocation = function(){
-    if( navigator.geolocation ){
-        // Call getCurrentPosition with success and failure callbacks
-        navigator.geolocation.getCurrentPosition( GP.success, GP.fail );
-    } else {
-        alert("Sorry, your browser does not support geolocation services.");
+    console.log('GP active');
+
+    var GP = {} || GP;
+
+    GP.initGeolocation = function(){
+        if( navigator.geolocation ){
+            // Call getCurrentPosition with success and failure callbacks
+            navigator.geolocation.getCurrentPosition( GP.success, GP.fail );
+        } else {
+            alert("Sorry, your browser does not support geolocation services.");
+        }
     }
-}
 
-GP.success = function(position){
-    console.log('long', position.coords.longitude);
-    console.log('lat', position.coords.latitude);
-}
+    GP.success = function(position){
+        console.log('long', position.coords.longitude);
+        console.log('lat', position.coords.latitude);
+    }
 
-GP.fail = function(){
-    // Could not obtain location
-}
+    GP.fail = function(){
+        // Could not obtain location
+    }
 
-$('.GP_get_location').on('click', function(e){
-    e.preventDefault();
-    GP.initGeolocation();
-});
+    $('.GP_get_location').on('click', function(e){
+        e.preventDefault();
+        GP.initGeolocation();
+    });
+})();
