599053b898f34b98584c066140481e8e7e818720
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-07 21:35:58 -0400

    Save the lat lon

--- app/assets/javascripts/GP-current-location.js
+++ app/assets/javascripts/GP-current-location.js
@@ -13,17 +13,22 @@ var GP = {} || GP;
     }
 
     GP.success = function(position){
-        console.log('long', position.coords.longitude);
-        console.log('lat', position.coords.latitude);
+        GP.lon = position.coords.longitude;
+        GP.lat = position.coords.latitude;
     }
 
     GP.fail = function(){
         // Could not obtain location
     }
 
+    GP.processSearch = function(){
+        location.href = location.origin + '/find?search=' + GP.lat + '%2C' + GP.lon;
+    }
+
     $('.GP_get_location').on('click', function(e){
         console.log('Clicked');
         GP.initGeolocation();
         e.preventDefault();
+        GP.processSearch();
     });
 })();
