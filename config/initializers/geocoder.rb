ba5e1469b54a4a918d40ad41fddf595fd5ea01b4
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-28 11:53:52 -0400

    Cahnge api

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
 Geocoder.configure(
   # geocoding options
-  :timeout      => 5,           # geocoding service timeout (secs)
+  # :timeout      => 5,           # geocoding service timeout (secs)
   :lookup       => :google,     # name of geocoding service (symbol)
   # :language     => :en,         # ISO-639 language code
   # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
