e45d8b884ddeeb66792abbc1701a3d03ea1a0762
RememberLenny <leonard_bogdonoff@condenast.com>
2014-11-02 21:48:33 -0500

    Save the color and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # resources :instagram_arts, :concerns => :paginatable
 
+  get 'slide' => 'instagram_arts#iosview'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
