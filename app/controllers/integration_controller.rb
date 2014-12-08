419a6a4080c2756ab53149b6a35fa0e2b1fa7dec
RememberLenny <leonard_bogdonoff@condenast.com>
2014-12-08 09:45:12 -0500

    Create the oauth code and the callback

--- app/controllers/integration_controller.rb
+++ app/controllers/integration_controller.rb
@@ -4,9 +4,13 @@ class IntegrationController < ApplicationController
   end
 
   def instagramOauthCallback
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    session[:access_token] = response.access_token
+    redirect root_path
   end
 
   def instagramOauthConnect
+    redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
   end
 
   def instagramResponse
