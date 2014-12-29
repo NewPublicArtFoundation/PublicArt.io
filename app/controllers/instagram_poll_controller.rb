931cd51d3d3434e8555d1f5e08416644869be0ed
rememberlenny <rememberlenny@gmail.com>
2014-12-29 16:11:16 -0500

    make the controllers more logical

--- app/controllers/instagram_poll_controller.rb
+++ app/controllers/instagram_poll_controller.rb
@@ -1,22 +1,22 @@
 class InstagramPollController < ApplicationController
 
-  def instagram_oauth_connect
+  def oauth_connect
     redirect_to Instagram.authorize_url(:redirect_uri => ENV['INSTAGRAM_CALLBACK_URL'])
   end
 
-  def instagram_oauth_callback
+  def oauth_callback
     response = Instagram.get_access_token(params[:code], :redirect_uri => ENV['INSTAGRAM_CALLBACK_URL'])
     session[:access_token] = response.access_token
     redirect_to root_url
   end
 
-  def instagram_realtime_callback
+  def realtime_callback
     if params["hub.challenge"]
       render :text => params["hub.challenge"]
     end
   end
 
-  def instagram_realtime_response
+  def realtime_response
     render :nothing => true, :status => 200, :content_type => 'text/html'
   end
 
