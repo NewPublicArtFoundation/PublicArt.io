77e3207357b2c3db77e4a9c8d6ecd6319b2e74d7
rememberlenny <rememberlenny@gmail.com>
2014-12-28 19:17:05 -0500

    Add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.app_domain = 'locahost:3000'
 
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'someuser',
+    password: 'somepass',
+    authentication: :plain,
+    domain: 'locahost:3000'
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.app_domain = 'http://www.publicart.io'
 
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: ENV["PUBLICART_GMAIL_UR"],
+    password: ENV["PUBLICART_GMAIL_PW"],
+    authentication: :plain,
+    domain: 'gmail.com'
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
