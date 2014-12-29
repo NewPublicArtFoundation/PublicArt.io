0a9b7c332d5cfae0d018a923bb1a79938842f4fe
rememberlenny <rememberlenny@gmail.com>
2014-12-29 11:14:58 -0500

    Do changes to the pa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :accesskeys
-
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   get 'api/instagram'
   get 'integration/instagram'
   get 'integration/flickr'
