7b6b5a409c87bb65c8b96ef61abb9467948ab0f2
rememberlenny <rememberlenny@gmail.com>
2014-12-29 23:50:13 -0500

    Save the new rate of hash list

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,8 +1,9 @@
 class GraphsController < ApplicationController
 
-  def rate_of_new
+  def rate_of_hash_over_year
     @per_day = []
-    for i in 1...365
+    for j in 1...365
+      i = 365-j
       day = {}
       now = Time.zone.now.beginning_of_day
       time = now.ago i*86400
@@ -10,7 +11,12 @@ class GraphsController < ApplicationController
       date = Time.zone.now.beginning_of_day.ago i*86400
       day["count_per_day"] = InstagramArt.where(:created_at => time.strftime("%F")..time_before.strftime("%F")).count
       day["day_per_count"] = time
-      @per_day << day
+      r = RateOfHash.new(:day_of => YAML::dump(day))
+      r.save
     end
+  end
+
+  def rate_of_new
+    @per_day = RateOfHash.all
   end
 end
