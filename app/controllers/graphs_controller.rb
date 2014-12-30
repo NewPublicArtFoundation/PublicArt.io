658e23a1207ffd81358f2be44aa5f8164d757f49
rememberlenny <rememberlenny@gmail.com>
2014-12-30 00:15:49 -0500

    SAve the graphs controller

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,13 +1,16 @@
 class GraphsController < ApplicationController
 
-  def rate_of_hash_yesterday
+  def check_rate_of_hash day_to_check
     day = {}
-    now = Time.zone.now.beginning_of_day.ago 86400
-    time = now.ago 86400
-    time_before = time.ago 86400
+    time_before = day_to_check
+    time = time.advance 86400
     date = Time.zone.now.beginning_of_day.ago 86400
     day["count_per_day"] = InstagramArt.where(:created_at => time_before.strftime("%F")..time.strftime("%F")).count
-    day["day_per_count"] = time
+    day["day_per_count"] = time_before
+    return day
+  end
+
+  def save_rate_of_hash day
     r = RateOfHash.new(:day_of => YAML::dump(day))
     r.save
   end
