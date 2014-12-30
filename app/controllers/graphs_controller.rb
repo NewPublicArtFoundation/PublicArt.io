1bf6989913276d0d86865134366fdb36d9a9a7ee
rememberlenny <rememberlenny@gmail.com>
2014-12-29 23:55:37 -0500

    give a yaml loader

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,19 +1,15 @@
 class GraphsController < ApplicationController
 
-  def rate_of_hash_over_year
-    @per_day = []
-    for j in 1...365
-      i = 365-j
-      day = {}
-      now = Time.zone.now.beginning_of_day
-      time = now.ago i*86400
-      time_before = time.ago 86400
-      date = Time.zone.now.beginning_of_day.ago i*86400
-      day["count_per_day"] = InstagramArt.where(:created_at => time.strftime("%F")..time_before.strftime("%F")).count
-      day["day_per_count"] = time
-      r = RateOfHash.new(:day_of => YAML::dump(day))
-      r.save
-    end
+  def rate_of_hash_yesterday
+    day = {}
+    now = Time.zone.now.beginning_of_day.ago 86400
+    time = now.ago 86400
+    time_before = time.ago 86400
+    date = Time.zone.now.beginning_of_day.ago 86400
+    day["count_per_day"] = InstagramArt.where(:created_at => time_before.strftime("%F")..time.strftime("%F")).count
+    day["day_per_count"] = time
+    r = RateOfHash.new(:day_of => YAML::dump(day))
+    r.save
   end
 
   def rate_of_new
