4eeab60561b70fb92e0ecfbabdd80a74d1474400
rememberlenny <rememberlenny@gmail.com>
2014-12-31 15:01:20 -0500

    Set up the individual are page

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,7 +1,10 @@
 class ArtsController < ApplicationController
   def show
+    uid = params[:id]
+    @graffiti = InstagramArts.where(:uid => uid)
   end
 
   def index
+    @graffiti = InstagramArt.page params[:page]
   end
 end
