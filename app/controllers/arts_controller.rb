d3e019b0c058193f109fddc3753761e3425a8227
rememberlenny <rememberlenny@gmail.com>
2014-12-31 16:13:18 -0500

    Change the way that map data is stored

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -8,4 +8,30 @@ class ArtsController < ApplicationController
   def index
     @graffiti = InstagramArt.page params[:page]
   end
+
+  def get_aggregate_map_data
+    map_datas = []
+    InstagramArt.find_each(batch_size: 2000) do |art|
+
+      map_data => {
+          :type => 'Feature',
+          :geometry => {
+              :type => 'Point',
+              :coordinates => [
+                art.longitude,
+                art.latitude
+              ]
+          },
+          :properties => {
+              :title => art.user_name + "'s photo",
+              :description => art.longitude + ', ' + art.latitude,
+              'marker-size' => 'small',
+              'marker-color' => '#BE9A6B',
+              :iconUrl => art.image_url,
+              :iconSize => [50, 50]
+          }
+      }
+      map_datas << map_data
+    end
+  end
 end
