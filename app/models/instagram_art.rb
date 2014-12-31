9ec7593813490ab500a5bbf1c34ce8c52fc3182c
rememberlenny <rememberlenny@gmail.com>
2014-12-31 00:35:06 -0500

    change the uid

--- app/models/instagram_art.rb
+++ app/models/instagram_art.rb
@@ -8,7 +8,7 @@ class InstagramArt < ActiveRecord::Base
       # This only works before_create obviously, otherwise it would
       # find itself and loop eternally.
       while self.uid.blank? or !InstagramArt.find_by_uid(self.uid).blank?
-        self.uid = Digest::SHA1.hexdigest("--#{self.title}--#{Time.current.usec}--")
+        self.uid = Digest::SHA1.hexdigest("--#{self.image_url}--#{Time.current.usec}--")
       end
     end
 end
