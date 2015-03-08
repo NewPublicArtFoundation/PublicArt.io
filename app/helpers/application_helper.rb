62529673e99f8be2bed1dc1ca06ec5a53a98c260
rememberlenny <rememberlenny@gmail.com>
2015-03-08 14:15:58 -0400

    Save the new thanks block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def thanks_block name, url, reason
+    content = '<li><a href="' + url + '"><div><h5>' + name + '</h5><em>' + reason + '</em></div></a></li>'
+    return content.html_safe
+  end
+
 end
