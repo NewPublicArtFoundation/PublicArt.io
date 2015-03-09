ecafe3c8ef0ce77a8e346c0635d0c647d3057a58
rememberlenny <rememberlenny@gmail.com>
2015-03-08 23:06:49 -0400

    All html safe string

--- app/helpers/instagram_arts_helper.rb
+++ app/helpers/instagram_arts_helper.rb
@@ -1,7 +1,7 @@
 module InstagramArtsHelper
 
   def graffiti_block_list_item url
-    html = '<li class="text-center">' + image_tag graffiti.image_url + '</li>'
-    return html
+    html = '<li class="text-center">' + image_tag(url) + '</li>'
+    return html.html_safe
   end
 end
