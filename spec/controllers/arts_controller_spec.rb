bb5d6c0d6217a3a9967de19953113f44d5646c57
Mário  Nzualo <mario.nzualo@gmail.com>
2015-03-25 07:51:45 +0000

    Fix arts controller spec

--- spec/controllers/arts_controller_spec.rb
+++ spec/controllers/arts_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe ArtsController, :type => :controller do
 
   describe "GET show" do
     it "returns http success" do
-      get :show
+      art = create(:instagram_art)
+      get :show, id: art.id
       expect(response).to have_http_status(:success)
     end
   end
---
+++ spec/factories/instagram_arts.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :instagram_art do
+    caption "This is art"
+    address "High street"
+  end
+end
