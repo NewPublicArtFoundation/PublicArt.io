48a8c68abb029f3568c990262dd81803b6abb7c2
rememberlenny <rememberlenny@gmail.com>
2015-03-27 09:36:23 -0400

    Save the UID"

--- app/helpers/instagram_arts_helper.rb
+++ app/helpers/instagram_arts_helper.rb
@@ -1,6 +1,7 @@
 module InstagramArtsHelper
-  def graffiti_block_list_item url
-    html = '<li class="text-center">' + image_tag(url) + '</li>'
+  def graffiti_block_list_item id
+    graffiti = InstagramArt.find(id)
+    html = '<li data-id="<%= graffiti.uid %>" class="text-center">' + image_tag(graffiti.image_url) + '</li>'
     return html.html_safe
   end
 end
