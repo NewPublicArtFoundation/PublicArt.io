494fcc4c8fee0cde7dc68c101d2c10482a209833
rememberlenny <rememberlenny@gmail.com>
2015-03-27 09:49:11 -0400

    Link dont work

--- app/helpers/instagram_arts_helper.rb
+++ app/helpers/instagram_arts_helper.rb
@@ -1,7 +1,7 @@
 module InstagramArtsHelper
   def graffiti_block_list_item id
     graffiti = InstagramArt.find(id)
-    html = '<li data-id="' + graffiti.uid + '" class="text-center">' + link_to image_tag(graffiti.image_url), graffiti + '</li>'
+    html = '<li data-id="' + graffiti.uid + '" class="text-center">' + image_tag(graffiti.image_url) + '</li>'
     return html.html_safe
   end
 end
