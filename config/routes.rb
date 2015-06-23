6a717a5143ce7a2e7b9aae7567b01b81095b2795
rememberlenny <rememberlenny@gmail.com>
2015-06-23 19:12:58 -0400

    Create a scope

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
+
   get 'api/instagram'
   get 'integration/instagram'
   get 'integration/flickr'
@@ -46,4 +47,10 @@ Rails.application.routes.draw do
   root 'pages#home_web'
 
   get "*any", via: :all, to: "errors#not_found" #404
+
+  scope :api do
+    # Login/Reg
+    devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+    match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  end
 end
