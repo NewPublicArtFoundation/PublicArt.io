2729e64b6988c00534582992b2ad94683fb8ae64
rememberlenny <rememberlenny@gmail.com>
2015-06-25 10:55:05 -0400

    Fix up the aPI

--- app/controllers/api/v2/instagram_arts_controller.rb
+++ app/controllers/api/v2/instagram_arts_controller.rb
@@ -19,26 +19,23 @@ module Api
         if params[:search].present?
           next_page = 1
           if params[:distance].present?
-            @distance_from = params[:distance]
+            distance_from = params[:distance]
           else
-            @distance_from = 5
+            distance_from = 5
           end
           if params[:page].present?
             next_page = params[:page].to_i + 1
           end
-          @search_url = 'http://www.graffpass.com/find.json/?search=' + params[:search] + "&page=" + next_page.to_s
+          search_url = 'http://www.graffpass.com/api/find.json/?search=' + URI.encode(params[:search]) + "&page=" + next_page.to_s
 
           if params[:orderby].present? && params[:orderby] == 'created_at'
-            @instagram_arts = InstagramArt.order('created_at DESC').near(params[:search], @distance_from).where(flagged: nil).page params[:page]
-            @result_count = InstagramArt.order('created_at DESC').near(params[:search], @distance_from).where(flagged: nil).count(:all)
+            instagram_arts = InstagramArt.order('created_at DESC').near(params[:search], distance_from).where(flagged: nil).page params[:page]
+            result_count = InstagramArt.order('created_at DESC').near(params[:search], distance_from).where(flagged: nil).count(:all)
           else
-            @instagram_arts = InstagramArt.near(params[:search], @distance_from).where(flagged: nil).page params[:page]
-            @result_count = InstagramArt.near(params[:search], @distance_from).where(flagged: nil).count(:all)
+            instagram_arts = InstagramArt.near(params[:search], distance_from).where(flagged: nil).page params[:page]
+            result_count = InstagramArt.near(params[:search], distance_from).where(flagged: nil).count(:all)
           end
-          @result_coordinates = Geocoder.coordinates(params[:search])
-          instagram_arts = @instagram_arts
-          the_params = params
-          search_url = @search_url
+          result_coordinates = Geocoder.coordinates(params[:search])
 
           if(params.has_key?(:page))
             page_count = params[:page].to_i
@@ -51,35 +48,32 @@ module Api
           len = instagram_arts.length
           items = IndexLocation.get_response_items instagram_arts
           result = {}
-          result[:next] = @search_url.html_safe
-          result[:count] = @result_count
+          result[:next] = search_url.html_safe
+          result[:count] = result_count
           result[:low] = page_range_low
           result[:high] = page_range_low + 50
-          response = {
+          @response = {
+            location: result_coordinates,
             search_term: params[:search],
             page_number: page_count,
-            page_total: @instagram_arts.total_pages,
+            page_total: instagram_arts.total_pages,
             result: result,
             data: items
           }
 
           respond_to do |format|
-            format.html {
+            format.json {
               render :indexlocation
             }
-            format.json {
-              data = response
-              render json: data,
-                     :content_type => 'application/json'
-            }
+            # format.json {
           end
         end
       end
 
       # GET /instagram_arts/:id/image
       def image
-        @instagram_arts = InstagramArt.find(params[:id])
-        render json: { id: params[:id], image: @instagram_arts.image_url }
+        instagram_arts = InstagramArt.find(params[:id])
+        render json: { id: params[:id], image: instagram_arts.image_url }
       end
 
     end
